86997737fea705f7dc14ceb086d78291357d109d
fdzrn <fadzriangibran@gmail.com>
2019-07-31 14:39:46 +0700

    Tugas 6 javascript
    Tugas ini saya kumpulkan kepada niomic

---
+++ tugas6.js
@@ -0,0 +1,11 @@
+function tugasEnam() {
+	for (var i = 1; i <= 20; i++) {
+		if (i % 2 == 0) {
+			console.log( i + ' = Genap');
+		} else {
+			console.log(i + ' = Ganjil');
+		}
+	}
+}
+
+tugasEnam();
